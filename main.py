e3de6ad8dd0b76995ad9df14855fd94d7cba1942
Hoang Luong <hoangluong@zaitenllc.com>
2021-04-18 12:42:12 +0700

    Move SpeechRecognizer to service

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ from tkinter import *
 from tkinter import messagebox as tkMessageBox
 import tkinter as tki
 from tkinter import filedialog as th1
-import pyttsx3
-import speech_recognition as sr
 import re
-import threading
 import time
 from config import config as cfg
 from model.Data import Data
+from service.SpeechRecognition import SpeechRecognizer
 import tkinter as tk
 API_KEY = cfg.credential['API_KEY']
 PROJECT_TOKEN = cfg.credential['PROJECT_TOKEN']
@@ -33,66 +31,6 @@ COUNTRY_PATTERNS = {
 				}
 
 UPDATE_COMMAND = "update"
-
-class SpeechRecognizer(threading.Thread):
-    
-    def speak(self, text):
-        engine = pyttsx3.init()
-        engine.say(text)
-        engine.runAndWait()
-
-
-    def get_audio(self):
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            r.adjust_for_ambient_noise(source)         # listen for 1 second to calibrate the energy threshold for ambient noise levels
-            self.recognized_text += "Listening...\n"
-
-            audio = r.listen(source)
-            said = ""
-
-            try:
-                said = r.recognize_google(audio)
-            except Exception as e:
-                print("Exception:", str(e))
-
-        return said.lower()
-
-    def __init__(self):
-        super(SpeechRecognizer, self).__init__()
-        self.setDaemon(True)
-        self.recognized_text = "Initializing\n"
-
-    def run(self):
-        while True:
-            # print("Listening...\n")
-            text = self.get_audio()
-            self.recognized_text += text + "\n"
-            result = None
-
-            for pattern, func in COUNTRY_PATTERNS.items():
-                if pattern.match(text):
-                    words = set(text.split(" "))
-                    for country in country_list:
-                        if country in words:
-                            result = func(country)
-                            break
-
-            for pattern, func in TOTAL_PATTERNS.items():
-                if pattern.match(text):
-                    result = func()
-                    break
-
-            if text == UPDATE_COMMAND:
-                result = "Data is being updated. This may take a moment!"
-                data.update_data()
-
-            if result:
-                self.speak(result)
-
-            if text.find(END_PHRASE) != -1:  # stop loop
-                self.recognized_text += "Exit\n"
-
 
 recognizer = SpeechRecognizer()
 recognizer.start()
